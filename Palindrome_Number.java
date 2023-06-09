5e8b6ebe049b440f40ebd9b62b36a853fae52c20
Rishabh Mathur <84562983+RishabhMathur-2404@users.noreply.github.com>
2023-06-09 08:48:17 +0530

    Add files via upload

---
+++ Palindrome_Number.java
@@ -0,0 +1,15 @@
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+public class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int number = x;
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return x == reverse;
+    }
+}
